09a713b462d873273b7e29d89771d231cdf0d79c
Martin Lücke <martin.luecke@ed.ed.ac.uk>
2020-06-26 18:55:14 +0100

    extending builders, introducing slide2d higher level abstraction

--- mlir/include/mlir/Dialect/Rise/EDSC/Builders.h
+++ mlir/include/mlir/Dialect/Rise/EDSC/Builders.h
@@ -16,6 +16,7 @@
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Rise/IR/Dialect.h"
+#include "mlir/Dialect/StandardOps/EDSC/Intrinsics.h"
 #include "mlir/EDSC/Builders.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Types.h"
@@ -24,7 +25,7 @@ using namespace mlir::rise;
 
 namespace mlir {
 namespace edsc {
-
+// arrayT
 namespace type {
 FunType funtype(Type in, Type out);
 Nat nat(int val);
@@ -40,8 +41,34 @@ ScalarType scalarF32();
 ScalarType scalarF64();
 } // namespace type
 
-namespace op {
+namespace abstraction {
+Value sumLambda(ScalarType summandType);
+Value multAndSumUpLambda(ScalarType summandType);
+Value slide2d(Nat szOuter, Nat stOuter, Nat szInner, Nat stInner,
+              Value array2DVal);
 
+} // namespace abstraction
+
+namespace op {
+using lambdaBodyBuilder = function_ref<Value(MutableArrayRef<BlockArgument>)>;
+
+// frontend edsc
+Value padClamp(Nat l, Nat r, Value array);
+Value slide(Nat sz, Nat sp, Value array);
+Value fst(Value tuple);
+Value snd(Value tuple);
+Value zip(Value lhs, Value rhs);
+Value transpose(Value array);
+Value mapSeq(StringRef lowerTo, DataType s, DataType t, Value lambda,
+             Value array);
+Value mapSeq(StringRef lowerTo, DataType s, DataType t,
+             lambdaBodyBuilder bodyBuilder,
+             Value array);
+//Value reduceSeq(Value lambda, Value initializer, Value array);
+Value reduceSeq(StringRef lowerTo, Value lambda, Value initializer,
+                Value array);
+Value reduceSeq(StringRef lowerTo, DataType s, DataType t, lambdaBodyBuilder bodyBuilder, Value initializer,
+                Value array);
 
 Value in(Value in, Type type);
 Value literal(DataType t, StringRef literal);
@@ -55,13 +82,32 @@ Value transpose(Nat n, Nat m, DataType t);
 Value transpose(Nat n, Nat m, DataType t, Value array);
 Value slide(Nat n, Nat sz, Nat sp, DataType t);
 Value slide(Nat n, Nat sz, Nat sp, DataType t, Value array);
-Value lambda(FunType lambdaType, function_ref<Value(MutableArrayRef<BlockArgument>)> bodyBuilder);
-//Value embed(Type result, ValueRange exposedValues, function_ref<void(MutableArrayRef<BlockArgument>)> bodyBuilder);
-Value embed(Type result, ValueRange exposedValues, function_ref<Value(MutableArrayRef<BlockArgument>)> bodyBuilder);
+Value padClamp(Nat n, Nat l, Nat r, DataType t);
+Value padClamp(Nat n, Nat l, Nat r, DataType t, Value array);
+Value lambda(FunType lambdaType,
+             lambdaBodyBuilder bodyBuilder);
+// TODO: Lambda with named args
+//Value mlir::edsc::op::lambda(
+//    FunType lambdaType,
+//    function_ref<Value(BlockArgument, BlockArgument)> bodyBuilder);
+//
+//Value mlir::edsc::op::lambda(
+//    FunType lambdaType,
+//    function_ref<Value(BlockArgument, BlockArgument, BlockArgument)> bodyBuilder);
+//
+//Value mlir::edsc::op::lambda(
+//    FunType lambdaType,
+//    function_ref<Value(BlockArgument, BlockArgument, BlockArgument, BlockArgument)> bodyBuilder);
+// Value embed(Type result, ValueRange exposedValues,
+// function_ref<void(MutableArrayRef<BlockArgument>)> bodyBuilder);
+Value embed(Type result, ValueRange exposedValues,
+            function_ref<Value(MutableArrayRef<BlockArgument>)> bodyBuilder);
 Value mapSeq(StringRef lowerTo, Nat n, DataType s, DataType t);
-Value mapSeq(StringRef lowerTo, Nat n, DataType s, DataType t, Value lambda, Value array);
+Value mapSeq(StringRef lowerTo, Nat n, DataType s, DataType t, Value lambda,
+             Value array);
 Value reduceSeq(StringRef lowerTo, Nat n, DataType s, DataType t);
-Value reduceSeq(StringRef lowerTo, Nat n, DataType s, DataType t, Value lambda, Value initializer, Value array);
+Value reduceSeq(StringRef lowerTo, Nat n, DataType s, DataType t, Value lambda,
+                Value initializer, Value array);
 
 void rise_return(Value returnValue);
 void out(Value writeTo, Value result);
--- mlir/lib/Dialect/Rise/EDSC/Builders.cpp
+++ mlir/lib/Dialect/Rise/EDSC/Builders.cpp
@@ -8,11 +8,15 @@
 
 #include "mlir/Dialect/Rise/EDSC/Builders.h"
 #include "mlir/Dialect/StandardOps/EDSC/Builders.h"
+#include "mlir/Dialect/StandardOps/EDSC/Intrinsics.h"
+#include <iostream>
 
 #include "mlir/IR/AffineExpr.h"
 #include "mlir/IR/AffineMap.h"
 
 using namespace mlir::rise;
+using namespace mlir::edsc::type;
+using namespace mlir::edsc::op;
 
 namespace mlir {
 namespace edsc {
@@ -34,19 +38,23 @@ ArrayType mlir::edsc::type::array(int size, DataType elemType) {
   return array(nat(size), elemType);
 }
 
-ArrayType mlir::edsc::type::array2D(Nat outerSize, Nat innerSize, DataType elemType) {
+ArrayType mlir::edsc::type::array2D(Nat outerSize, Nat innerSize,
+                                    DataType elemType) {
   return array(outerSize, array(innerSize, elemType));
 }
 
-ArrayType mlir::edsc::type::array2D(int outerSize, int innerSize, DataType elemType) {
-    return array2D(nat(outerSize), nat(innerSize), elemType);
+ArrayType mlir::edsc::type::array2D(int outerSize, int innerSize,
+                                    DataType elemType) {
+  return array2D(nat(outerSize), nat(innerSize), elemType);
 }
 
-ArrayType mlir::edsc::type::array3D(Nat outerSize, Nat midSize, Nat innerSize, DataType elemType) {
+ArrayType mlir::edsc::type::array3D(Nat outerSize, Nat midSize, Nat innerSize,
+                                    DataType elemType) {
   return array2D(outerSize, midSize, array(innerSize, elemType));
 }
 
-ArrayType mlir::edsc::type::array3D(int outerSize, int midSize, int innerSize, DataType elemType) {
+ArrayType mlir::edsc::type::array3D(int outerSize, int midSize, int innerSize,
+                                    DataType elemType) {
   return array2D(outerSize, midSize, array(nat(innerSize), elemType));
 }
 
@@ -98,6 +106,25 @@ Value mlir::edsc::op::reduceSeq(StringRef lowerTo, Nat n, DataType s,
                                ValueRange{lambda, initializer, array});
 }
 
+Value mlir::edsc::op::reduceSeq(StringRef lowerTo, Value lambda,
+                                Value initializer, Value array) {
+  ArrayType arrayType = array.getType().dyn_cast<ArrayType>();
+  return reduceSeq(lowerTo, arrayType.getSize(), arrayType.getElementType(),
+                   arrayType.getElementType(), lambda, initializer, array);
+}
+
+Value mlir::edsc::op::reduceSeq(
+    StringRef lowerTo, DataType s, DataType t,
+    function_ref<Value(MutableArrayRef<BlockArgument>)> bodyBuilder,
+    Value initializer, Value array) {
+  ArrayType arrayType = array.getType().dyn_cast<ArrayType>();
+
+  return reduceSeq(lowerTo, arrayType.getSize(), arrayType.getElementType(),
+                   arrayType.getElementType(),
+                   lambda(funtype(s, funtype(s, t)), bodyBuilder), initializer,
+                   array);
+}
+
 Value mlir::edsc::op::zip(Nat n, DataType s, DataType t) {
   MLIRContext *context = ScopedContext::getContext();
   FunType zipType = FunType::get(
@@ -118,6 +145,14 @@ Value mlir::edsc::op::zip(Nat n, DataType s, DataType t, Value lhs, Value rhs) {
       ValueRange{lhs, rhs});
 }
 
+Value mlir::edsc::op::zip(Value lhs, Value rhs) {
+  MLIRContext *context = ScopedContext::getContext();
+  ArrayType lhsType = lhs.getType().dyn_cast<ArrayType>();
+  ArrayType rhsType = rhs.getType().dyn_cast<ArrayType>();
+  return zip(lhsType.getSize(), lhsType.getElementType(),
+             rhsType.getElementType(), lhs, rhs);
+}
+
 Value mlir::edsc::op::fst(DataType s, DataType t) {
   MLIRContext *context = ScopedContext::getContext();
   return ValueBuilder<FstOp>(
@@ -130,6 +165,11 @@ Value mlir::edsc::op::fst(DataType s, DataType t, Value tuple) {
   return ValueBuilder<ApplyOp>(s, fstOp, tuple);
 }
 
+Value mlir::edsc::op::fst(Value tuple) {
+  Tuple tupleType = tuple.getType().dyn_cast<Tuple>();
+  return fst(tupleType.getFirst(), tupleType.getSecond(), tuple);
+}
+
 Value mlir::edsc::op::snd(DataType s, DataType t) {
   MLIRContext *context = ScopedContext::getContext();
   return ValueBuilder<SndOp>(
@@ -140,6 +180,11 @@ Value mlir::edsc::op::snd(DataType s, DataType t) {
 Value mlir::edsc::op::snd(DataType s, DataType t, Value tuple) {
   Value sndOp = snd(s, t);
   return ValueBuilder<ApplyOp>(t, sndOp, tuple);
+}
+
+Value mlir::edsc::op::snd(Value tuple) {
+  Tuple tupleType = tuple.getType().dyn_cast<Tuple>();
+  return snd(tupleType.getFirst(), tupleType.getSecond(), tuple);
 }
 
 Value mlir::edsc::op::transpose(Nat n, Nat m, DataType t) {
@@ -159,6 +204,16 @@ Value mlir::edsc::op::transpose(Nat n, Nat m, DataType t, Value array) {
   return ValueBuilder<ApplyOp>(
       ArrayType::get(context, m, ArrayType::get(context, n, t)), transposeOp,
       ValueRange{array});
+}
+
+Value mlir::edsc::op::transpose(Value array) {
+  MLIRContext *context = ScopedContext::getContext();
+  ArrayType outerArrayType = array.getType().dyn_cast<ArrayType>();
+  ArrayType innerArrayType =
+      outerArrayType.getElementType().dyn_cast<ArrayType>();
+
+  return transpose(innerArrayType.getSize(), outerArrayType.getSize(),
+                   innerArrayType.getElementType(), array);
 }
 
 Value mlir::edsc::op::slide(Nat n, Nat sz, Nat sp, DataType t) {
@@ -184,6 +239,51 @@ Value mlir::edsc::op::slide(Nat n, Nat sz, Nat sp, DataType t, Value array) {
       ValueRange{array});
 }
 
+Value mlir::edsc::op::slide(Nat sz, Nat sp, Value array) {
+  MLIRContext *context = ScopedContext::getContext();
+  ArrayType arrayType = array.getType().dyn_cast<ArrayType>();
+  int n = (arrayType.getSize().getIntValue() + sp.getIntValue() -
+           sz.getIntValue()) /
+          sp.getIntValue();
+
+  return slide(nat(n), sz, sp, arrayType.getElementType(), array);
+}
+
+Value mlir::edsc::op::padClamp(Nat n, Nat l, Nat r, DataType t) {
+  MLIRContext *context = ScopedContext::getContext();
+  FunType padType = FunType::get(
+      context, t,
+      FunType::get(
+          context, ArrayType::get(context, n, t),
+          ArrayType::get(context,
+                         Nat::get(context, l.getIntValue() + n.getIntValue() +
+                                               r.getIntValue()),
+                         t)));
+  return ValueBuilder<PadOp>(padType, NatAttr::get(context, n),
+                             NatAttr::get(context, l), NatAttr::get(context, r),
+                             DataTypeAttr::get(context, t));
+}
+
+Value mlir::edsc::op::padClamp(Nat n, Nat l, Nat r, DataType t, Value array) {
+  MLIRContext *context = ScopedContext::getContext();
+  Value cst0 = mlir::edsc::intrinsics::std_constant_float(
+      llvm::APFloat(7.0f), FloatType::getF32(context));
+
+  Value pad = padClamp(n, l, r, t);
+  return ValueBuilder<ApplyOp>(
+      ArrayType::get(context,
+                     Nat::get(context, l.getIntValue() + n.getIntValue() +
+                                           r.getIntValue()),
+                     t),
+      pad, ValueRange{cst0, array});
+}
+
+Value mlir::edsc::op::padClamp(Nat l, Nat r, Value array) {
+  MLIRContext *context = ScopedContext::getContext();
+  ArrayType arrayType = array.getType().dyn_cast<ArrayType>();
+  return padClamp(arrayType.getSize(), l, r, arrayType.getElementType(), array);
+}
+
 Value mlir::edsc::op::mapSeq(StringRef lowerTo, Nat n, DataType s, DataType t) {
   MLIRContext *context = ScopedContext::getContext();
   FunType mapType =
@@ -200,8 +300,21 @@ Value mlir::edsc::op::mapSeq(StringRef lowerTo, Nat n, DataType s, DataType t,
                              Value lambda, Value array) {
   MLIRContext *context = ScopedContext::getContext();
   Value mapSeq = mlir::edsc::op::mapSeq(lowerTo, n, s, t);
-  return ValueBuilder<ApplyOp>(ArrayType::get(context, n, s), mapSeq,
+  return ValueBuilder<ApplyOp>(ArrayType::get(context, n, t), mapSeq,
                                ValueRange{lambda, array});
+}
+
+Value mlir::edsc::op::mapSeq(StringRef lowerTo, DataType s, DataType t,
+                             Value lambda, Value array) {
+  ArrayType arrayType = array.getType().dyn_cast<ArrayType>();
+  return mapSeq(lowerTo, arrayType.getSize(), s, t, lambda, array);
+}
+
+Value mlir::edsc::op::mapSeq(
+    StringRef lowerTo, DataType s, DataType t,
+    function_ref<Value(MutableArrayRef<BlockArgument>)> bodyBuilder,
+    Value array) {
+  return mapSeq(lowerTo, s, t, lambda(funtype(s, t), bodyBuilder), array);
 }
 
 // TODO: maybe change the builder to automatically introduce a return for the
@@ -212,9 +325,9 @@ Value mlir::edsc::op::lambda(
   return ValueBuilder<LambdaOp>(
       lambdaType, [&](OpBuilder &nestedBuilder, Location nestedLoc,
                       MutableArrayRef<BlockArgument> args) {
-          ScopedContext nestedContext(nestedBuilder, nestedLoc);
-          OpBuilder::InsertionGuard guard(nestedBuilder);
-          return bodyBuilder(args);
+        ScopedContext nestedContext(nestedBuilder, nestedLoc);
+        OpBuilder::InsertionGuard guard(nestedBuilder);
+        return bodyBuilder(args);
       });
 }
 
@@ -223,8 +336,9 @@ Value mlir::edsc::op::lambda(
 // Value mlir::edsc::op::lambda(Type resultType, Type inType1, Type inType1,
 // function_ref<void(MutableArrayRef<BlockArgument>)> bodyBuilder) {
 
-// // I want to have an embed which does not return a Value in its body. However the template instantiation here is ambiguous for some reason.
-//Value mlir::edsc::op::embed(
+// // I want to have an embed which does not return a Value in its body. However
+// the template instantiation here is ambiguous for some reason.
+// Value mlir::edsc::op::embed(
 //    Type result, ValueRange exposedValues,
 //    function_ref<void(MutableArrayRef<BlockArgument>)> bodyBuilder) {
 //  return ValueBuilder<EmbedOp>(
@@ -245,10 +359,9 @@ Value mlir::edsc::op::embed(
       result, exposedValues,
       [&](OpBuilder &nestedBuilder, Location nestedLoc,
           MutableArrayRef<BlockArgument> args) -> Value {
-
-          ScopedContext nestedContext(nestedBuilder, nestedLoc);
-          OpBuilder::InsertionGuard guard(nestedBuilder);
-          return bodyBuilder(args);
+        ScopedContext nestedContext(nestedBuilder, nestedLoc);
+        OpBuilder::InsertionGuard guard(nestedBuilder);
+        return bodyBuilder(args);
       });
 }
 
@@ -262,43 +375,78 @@ void mlir::edsc::op::out(Value writeTo, Value result) {
   return;
 }
 
-// TODO: This is what affineLoopBuilder looks like. The function is directly
-// passed in the arguments here. Way better than before.
+// Abstractions
 
-// void mlir::edsc::op::lambdaBuilder(ValueRange lbs, ValueRange ubs, int64_t
-// step,
-//                                   function_ref<void(Value)> bodyBuilder) {
-//  // Fetch the builder and location.
-//  assert(ScopedContext::getContext() && "EDSC ScopedContext not set up");
-//  OpBuilder &builder = ScopedContext::getBuilderRef();
-//  Location loc = ScopedContext::getLocation();
+// How do I include everything here correctly to have + of Values overloaded?
+Value mlir::edsc::abstraction::sumLambda(ScalarType summandType) {
+  return lambda(
+      funtype(summandType, funtype(summandType, summandType)), [&](auto args) {
+        return (
+            embed(summandType, ValueRange{args[0], args[1]}, [&](auto args) {
+              return (mlir::edsc::intrinsics::std_addf(args[0], args[1]));
+            }));
+      });
+}
+
+Value mlir::edsc::abstraction::multAndSumUpLambda(ScalarType summandType) {
+  return lambda(
+      funtype(tuple(summandType, summandType),
+              funtype(summandType, summandType)),
+      [&](auto args) {
+        return (embed(
+            summandType,
+            ValueRange{fst(summandType, summandType, args[0]),
+                       snd(summandType, summandType, args[0]), args[1]},
+            [&](auto args) {
+              return (mlir::edsc::intrinsics::std_mulf(
+                  args[0], mlir::edsc::intrinsics::std_addf(args[1], args[2])));
+            }));
+      });
+}
+
+// ArrayType::get(context, n, ArrayType::get(context, sz, t))
+// Value mlir::edsc::abstraction::slide2d(Nat szOuter, Nat stOuter, Nat szInner,
+// Nat stInner, Value array2DVal) {
+//  ArrayType arrayType = array2DVal.getType().dyn_cast<ArrayType>();
+//  ArrayType innerArrayType = arrayType.getElementType().dyn_cast<ArrayType>();
 //
-//  // Create the actual loop and call the body builder, if provided, after
-//  // updating the scoped context.
-//  builder.create<AffineForOp>(
-//      loc, lbs, builder.getMultiDimIdentityMap(lbs.size()), ubs,
-//      builder.getMultiDimIdentityMap(ubs.size()), step,
-//      [&](OpBuilder &nestedBuilder, Location nestedLoc, Value iv) {
-//        if (bodyBuilder) {
-//          ScopedContext nestedContext(nestedBuilder, nestedLoc);
-//          OpBuilder::InsertionGuard guard(nestedBuilder);
-//          bodyBuilder(iv);
-//        }
-//        nestedBuilder.create<AffineTerminatorOp>(nestedLoc);
-//      });
+//
+//  mapSeq("loop", lambda(funtype(arrayType.getElementType(), array2D())))
+//  return
 //}
 
-// Value mapSeq(StringRef lowerTo, Nat n, DataType s, DataType t,
-// function_ref<Value(void)> createLambda, Value array) {
-//
-//  auto lambda = createLambda();
-//  Value mapSeq = mlir::edsc::op::mapSeq(n,s,t,lowerTo);
-//
-//  return ValueBuilder<ApplyOp>(
-//      ArrayType::get(context, n, s),
-//      mapSeq,
-//      ValueRange{lambda, array});
-//}
+Value mlir::edsc::abstraction::slide2d(Nat szOuter, Nat spOuter, Nat szInner,
+                                       Nat spInner, Value array2DVal) {
+  ArrayType arrayType = array2DVal.getType().dyn_cast<ArrayType>();
+  ArrayType innerArrayType = arrayType.getElementType().dyn_cast<ArrayType>();
+  int newInnerArraySize = (innerArrayType.getSize().getIntValue() +
+                           spInner.getIntValue() - szInner.getIntValue()) /
+                          spInner.getIntValue();
+  std::cout << "innernewsizeL" << newInnerArraySize << std::flush;
+  Value afterFirstSlide = mapSeq(
+      "loop", innerArrayType,
+      array(nat(newInnerArraySize),
+            array(szInner, innerArrayType.getElementType())),
+      [&](auto args) { return slide(szInner, spInner, args[0]); }, array2DVal);
+
+  Value afterSecondSlide = slide(szOuter, spOuter, afterFirstSlide);
+
+  ArrayType afterSecondSlideType =
+      afterSecondSlide.getType().dyn_cast<ArrayType>();
+  ArrayType afterSecondSlideElementType =
+      afterSecondSlideType.getElementType().dyn_cast<ArrayType>();
+  ArrayType afterSecondSlideElementElementType =
+      afterSecondSlideElementType.getElementType().dyn_cast<ArrayType>();
+
+  Value transposed = mapSeq("loop", afterSecondSlideElementType, array(afterSecondSlideElementElementType.getSize(), array(afterSecondSlideElementType.getSize(), afterSecondSlideElementElementType.getElementType())), [&](auto args){
+    return transpose(args[0]);
+  }, afterSecondSlide);
+
+  return transposed;
+}
+
+// def slide2D(szOuter: Nat, stOuter: Nat, szInner: Nat, stInner: Nat): Expr =
+// map(slide(szInner)(stInner)) >> slide(szOuter)(stOuter) >> map(transpose)
 
 } // namespace edsc
 } // namespace mlir
